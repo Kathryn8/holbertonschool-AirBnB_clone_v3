37ef6fd8040f80857673a701ad0c08c13cef5e0c
Kathryn6368 <6368@holbertonstudents.com>
2023-08-24 18:58:18 -0700

    add files for task 4

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+""" Create an instance of Blueprint """
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+""" Returns the status of API """
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """returst status:OK in json"""
+    return jsonify({"status": "OK"})
